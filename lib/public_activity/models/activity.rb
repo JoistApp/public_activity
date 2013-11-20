1d39fb33d6c9b9acc26d364000851dc34ddeddb9
Michael Gauthier <michael@joistapp.com>
2013-11-20 17:29:42 -0500

    Use regular expression for key demodulization

--- lib/public_activity/models/activity.rb
+++ lib/public_activity/models/activity.rb
@@ -3,10 +3,9 @@ module PublicActivity
     after_create :demodulize_record
 
     def demodulize_record
-      puts "++++++++++++++++++ DEMODULIZE RECORD +++++++++++++++"
       self.trackable_type ||= self.trackable_type.demodulize
       self.owner_type ||= self.owner_type.demodulize
-      self.key = self.key.gsub('v3_','')
+      self.key = self.key.gsub(/v\d+_/,'') #e.g. convert v3_invoice to invoice
       self.save
     end
   end
