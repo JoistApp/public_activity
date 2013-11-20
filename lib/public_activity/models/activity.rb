bf30aea4f8377a18b04c6343d994336b5bd17f06
Michael Gauthier <mikeg@joist-web.dev>
2013-11-20 17:14:09 -0500

    typo

--- lib/public_activity/models/activity.rb
+++ lib/public_activity/models/activity.rb
@@ -2,7 +2,7 @@ module PublicActivity
   class Activity < inherit_orm("Activity")
     after_create :demodulize_record
 
-    def :demodulize_record
+    def demodulize_record
       puts "++++++++++++++++++ DEMODULIZE RECORD +++++++++++++++"
     end
   end
