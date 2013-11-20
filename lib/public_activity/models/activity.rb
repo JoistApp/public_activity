7a912a13f2188d2b21b4657035c5173108301228
Michael Gauthier <mikeg@joist-web.dev>
2013-11-20 17:11:34 -0500

    Test

--- lib/public_activity/models/activity.rb
+++ lib/public_activity/models/activity.rb
@@ -1,4 +1,9 @@
 module PublicActivity
   class Activity < inherit_orm("Activity")
+    after_create :demodulize_record
+
+    def :demodulize_record
+      puts "++++++++++++++++++ DEMODULIZE RECORD +++++++++++++++"
+    end
   end
 end
