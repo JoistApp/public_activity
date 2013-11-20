756b6171d437193e778ded6f583116fd3a4ac0d6
Michael Gauthier <mikeg@joist-web.dev>
2013-11-20 17:19:43 -0500

    Implement demodulize

--- lib/public_activity/models/activity.rb
+++ lib/public_activity/models/activity.rb
@@ -4,6 +4,10 @@ module PublicActivity
 
     def demodulize_record
       puts "++++++++++++++++++ DEMODULIZE RECORD +++++++++++++++"
+      self.trackable_type ||= self.trackable_type.demodulize
+      self.owner_type ||= self.owner_type.demodulize
+      self.key = self.key.gsub('v3_','')
+      self.save
     end
   end
 end
